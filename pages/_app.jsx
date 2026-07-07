f86d054a3a4c853d43ddf40a84cc53e9a4461163
Atul <atul@1solutions.biz>
2026-07-07 19:05:32 +0530

    fix: replace invalid fallback og:image with correct URL and dimensions
    The non-www URL https://1solutions.biz/wp-content/uploads/... triggers
    a redirect that Facebook's crawler won't follow, causing the 'Invalid
    Image Content Type' warning. Replace with the self-hosted banner at
    https://www.1solutions.biz/images/banner-1sol.jpg (already in /public).
    Also correct the declared dimensions from 520x270 to 1200x630.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -31,9 +31,9 @@ export default function App({ Component, pageProps }) {
           <meta name="google-site-verification" content="252jFsFhtusIQZlJdn2HMUnYv9qz3uOlvXG4iQ9YdgY" />
           <meta name="twitter:site" content="@1solutionsbiz" />
           <meta property="article:publisher" content="https://www.facebook.com/1solutions" />
-          <meta property="og:image" content="https://1solutions.biz/wp-content/uploads/2025/06/1Solutions.jpg" />
-          <meta property="og:image:width" content="520" />
-          <meta property="og:image:height" content="270" />
+          <meta property="og:image" content="https://www.1solutions.biz/images/banner-1sol.jpg" />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
           <meta property="og:image:type" content="image/jpeg" />
         </Head>
         <Script id="gtm-init" strategy="lazyOnload">{`
@@ -56,9 +56,9 @@ export default function App({ Component, pageProps }) {
         <meta name="google-site-verification" content="252jFsFhtusIQZlJdn2HMUnYv9qz3uOlvXG4iQ9YdgY" />
         <meta name="twitter:site" content="@1solutionsbiz" />
         <meta property="article:publisher" content="https://www.facebook.com/1solutions" />
-        <meta property="og:image" content="https://1solutions.biz/wp-content/uploads/2025/06/1Solutions.jpg" />
-        <meta property="og:image:width" content="520" />
-        <meta property="og:image:height" content="270" />
+        <meta property="og:image" content="https://www.1solutions.biz/images/banner-1sol.jpg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
         <meta property="og:image:type" content="image/jpeg" />
       </Head>
       <Script id="gtm-init-default" strategy="lazyOnload">{`
